91eb03cec02938491d34682750806a6d4d223ae5
gabipiovezan <gabrielapiovezan@live.com>
2022-08-01 23:07:17 -0300

    Incluindo uma dupla validação

--- src/main/java/com/ead/course/validation/CourseValidation.java
+++ src/main/java/com/ead/course/validation/CourseValidation.java
@@ -1,10 +1,12 @@
 package com.ead.course.validation;
 
+import com.ead.course.config.security.AuthenticationCurrentUserService;
 import com.ead.course.dtos.CourseDTO;
 import com.ead.course.enums.UserType;
 import com.ead.course.models.UserModel;
 import com.ead.course.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -19,6 +21,8 @@ public class CourseValidation implements Validator {
     private final Validator validator;
 
     private final UserService userService;
+
+    private final AuthenticationCurrentUserService authenticationCurrentUserService;
 
     @Override
     public boolean supports(Class<?> clazz) {
@@ -35,12 +39,17 @@ public class CourseValidation implements Validator {
     }
 
     private void validateUserIstructor(UUID userId, Errors errors) {
-        Optional<UserModel> userModelOpt = userService.findById(userId);
-        userModelOpt.ifPresentOrElse(userModel -> {
-            if (!userModel.getUserType().equals(UserType.INSTRUCTOR.name())) {
-                errors.rejectValue("userInstructor", "userInstructorError", "User must be INSTRUCTOR or ADMIN.");
-            }
-        }, () -> errors.rejectValue("userInstructor", "UserInstructorError", "Instructor not fount."));
+        UUID currentUserId = authenticationCurrentUserService.getCurrentUser().getUserId();
 
+        if (currentUserId.equals(userId)) {
+            Optional<UserModel> userModelOpt = userService.findById(userId);
+            userModelOpt.ifPresentOrElse(userModel -> {
+                if (!userModel.getUserType().equals(UserType.INSTRUCTOR.name())) {
+                    errors.rejectValue("userInstructor", "userInstructorError", "User must be INSTRUCTOR or ADMIN.");
+                }
+            }, () -> errors.rejectValue("userInstructor", "UserInstructorError", "Instructor not fount."));
+        } else {
+            throw new AccessDeniedException("You are not allowed to do this action.");
+        }
     }
 }
